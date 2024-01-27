0050e0adc5e3b27819c0336fa998556f03265c0e
Andrei Popica <popica.andreivlad@gmail.com>
2024-01-27 23:07:16 +0200

    Api endpoints added

---
+++ server/greenhouse/src/main/java/com/iot/greenhouse/controller/GreenhouseController.java
@@ -0,0 +1,33 @@
+package com.iot.greenhouse.controller;
+
+import com.iot.greenhouse.model.GreenhouseMonitor;
+import com.iot.greenhouse.service.GreenhouseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/greenhouse")
+@RequiredArgsConstructor
+@Slf4j
+public class GreenhouseController {
+
+    public final GreenhouseService greenhouseService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void submitForm(@RequestBody GreenhouseMonitor greenhouseMonitor) {
+        greenhouseService.saveMonitor(greenhouseMonitor);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<GreenhouseMonitor> getAllForms(@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") Date fromDate) {
+       return greenhouseService.getAllMonitorsBefore(fromDate);
+    }
+}
