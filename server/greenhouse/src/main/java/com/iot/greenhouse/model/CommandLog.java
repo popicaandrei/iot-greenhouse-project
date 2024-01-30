05ed19892caf7ce79ef4ef65c555ab3343f02ce5
Andrei Popica <popica.andreivlad@gmail.com>
2024-01-30 16:47:21 +0200

    Add history for commands

---
+++ server/greenhouse/src/main/java/com/iot/greenhouse/model/CommandLog.java
@@ -0,0 +1,34 @@
+package com.iot.greenhouse.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommandLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column
+    private boolean heaterSwitch;
+
+    @Column
+    private boolean fanSwitch;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = true)
+    private Date timestamp;
+
+    @PrePersist
+    private void onCreate() {
+        timestamp = new Date(System.currentTimeMillis());
+    }
+}
---
+++ server/greenhouse/src/main/java/com/iot/greenhouse/repository/CommandLogRepository.java
@@ -0,0 +1,10 @@
+package com.iot.greenhouse.repository;
+
+import com.iot.greenhouse.model.CommandLog;
+import com.iot.greenhouse.model.GreenhouseMonitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandLogRepository extends JpaRepository<CommandLog, Integer> {
+}
